4dc42b06b2f6a9ac92f18683e6503f640afb7520
Italo Vinicius <italovinicius18@yahoo.com>
2022-06-27 17:25:50 -0300

    Verificação de erros e loop

--- main.hs
+++ main.hs
@@ -1,8 +1,12 @@
+import System.Exit (exitFailure, exitSuccess)
+
 checkOptions option = 
     if option == "1" then game
     else if option == "2" then ranking
-    else if option == "0" then return ()
-    else putStrLn "Opção inválida!"
+    else if option == "0" then exitSuccess
+    else do 
+        putStrLn "Opção inválida!"
+        main
 
 game = putStrLn "JOGANDO ..."
 
@@ -10,7 +14,7 @@ ranking = putStrLn "RANKING ..."
 
 main :: IO ()
 main = do 
-    
+    putStrLn "Bem-vindo ao jogo de Batalha Naval!"
     putStrLn "Escolha a opção desejada:"
     putStrLn "1. JOGAR"
     putStrLn "2. RANKING"
