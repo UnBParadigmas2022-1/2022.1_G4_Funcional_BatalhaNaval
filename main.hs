38208f1fd3fdbf5b9b8ea325921e894abb364870
AlvaroLeles <alvaro.leles@hotmail.com>
2022-06-29 19:31:39 -0300

    Adicionando leitura de nome do usuário para o ranking e tentativa de criação de tabuleiro

--- main.hs
+++ main.hs
@@ -1,4 +1,6 @@
 import System.Exit (exitFailure, exitSuccess)
+import Data.Char (ord)
+import Data.List (permutations)
 
 checkOptions option = 
     if option == "1" then game
@@ -8,17 +10,36 @@ checkOptions option =
         putStrLn "Opção inválida!"
         main
 
-game = putStrLn "JOGANDO ..."
+entraNomeJogador :: IO String
+entraNomeJogador = do
+    putStrLn "Nome para o ranking"
+    nome <- getLine
+    return nome
+
+imprimeTabuleiro = do
+    putStrLn (take (12) (repeat 'H') ++1 "\nH" ++ take (10) (repeat ' ') ++ "H\n" ++ take (12) (repeat 'H') )
+    putStrLn ""
+
+game = do
+    nomeJogador <- entraNomeJogador
+    
+    imprimeTabuleiro
+    
 
 ranking = putStrLn "RANKING ..."
 
-main :: IO ()
-main = do 
+apresentaMenu :: IO String
+apresentaMenu = do
     putStrLn "Bem-vindo ao jogo de Batalha Naval!"
     putStrLn "Escolha a opção desejada:"
     putStrLn "1. JOGAR"
     putStrLn "2. RANKING"
     putStrLn "0. SAIR"
     option <- getLine
+    return option
+
+main :: IO ()
+main = do
+    option <- apresentaMenu
     checkOptions option
-    main
+    -- main
